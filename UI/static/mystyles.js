d9248c1e86958bf17dcb62c519853033ee481d5f
codjoero <rownee2001@gmail.com>
2018-10-23 18:10:29 +0300

    ch(gh-pages update): Updated the gh-pages
    This updates the gh-pages to reflect changes in th UI
    
    [Deliveries #161419448]

--- UI/static/mystyles.js
+++ UI/static/mystyles.js
@@ -2,11 +2,11 @@
 var _CONFIG = {
     "user": {
         "Password": "andela",
-        "Redirect": "../templates/user/dashboard.html"
+        "Redirect": "/UI/templates/user/dashboard.html"
     },
     "admin": {
         "Password": "andela",
-        "Redirect": "../templates/admin/dashboard.html"
+        "Redirect": "/UI/templates/admin/dashboard.html"
     }
 }
 
