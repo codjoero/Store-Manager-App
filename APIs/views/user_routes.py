2f9a2247c6fabe286660f3fcc74afc215b8b119a
codjoero <rownee2001@gmail.com>
2018-10-30 15:44:07 +0300

    ft(register admin account): refactor to enables registration of admin
    This refactors code to allow addition of admin account
    
    [Deliveries #161572256]

--- APIs/views/user_routes.py
+++ APIs/views/user_routes.py
@@ -1,25 +1,25 @@
 from flask import Flask, request, jsonify, make_response
 from APIs import app
 import APIs.views
-from APIs.models.users import Users
+from APIs.models.users import User
 
-users = Users()
+# users = User()
 
 
-"""Users endpoints 
-"""
-@app.route('/api/v1/users', methods=['POST'])
-def create_user():
-    return users.create_user()
+# """Users endpoints 
+# """
+# @app.route('/api/v1/users', methods=['POST'])
+# def create_user():
+#     return users.create_user()
 
-@app.route('/api/v1/users', methods=['GET'])
-def view_all_users():
-    return users.view_all_users()
+# @app.route('/api/v1/users', methods=['GET'])
+# def view_all_users():
+#     return users.view_all_users()
 
-@app.route('/api/v1/users/<int:_id>', methods=['PUT'])
-def update_user(_id):
-    return users.update_user(_id)
+# @app.route('/api/v1/users/<int:_id>', methods=['PUT'])
+# def update_user(_id):
+#     return users.update_user(_id)
 
-@app.route('/api/v1/users/<int:_id>', methods=['DELETE'])
-def delete_user(_id):
-    return users.delete_user(_id)
+# @app.route('/api/v1/users/<int:_id>', methods=['DELETE'])
+# def delete_user(_id):
+#     return users.delete_user(_id)
