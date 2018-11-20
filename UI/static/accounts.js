da480102c23ebc2b507c3294cecb5a7b22f67a44
codjoero <rownee2001@gmail.com>
2018-11-20 21:49:13 +0300

    ft(admin edit attendant): Creates functionality for editing a user account
    This allows admin to change a store attendant account.
    [Deliveries #162042598]

--- UI/static/accounts.js
+++ UI/static/accounts.js
@@ -1,6 +1,7 @@
 //Declarations
 const usersUrl = 'https://thecodestoremanager-api-heroku.herokuapp.com/api/v1/users'
 const msg = document.querySelector('span.msg')
+const updateMsg = document.getElementById('updateMsg')
 const errMsg = document.querySelector('span.errMsg')
 const token = localStorage.getItem("token");
 const adminLoggedin = localStorage.getItem("adminLoggedin");
@@ -10,6 +11,8 @@ const mytableBody = document.querySelector('#mytable > tbody');
 // Listeners
 document.getElementById('addUser').addEventListener
 ('submit', addUser)
+document.getElementById('updateUser').addEventListener
+('submit', updateUser)
 
 const errHandler = (response) => {
     if (!response.ok){
@@ -39,14 +42,65 @@ function addUser(e){
             name:name, username:username, password:password, role:role
         })
     })
-    .then(errHandler)
-    .then((res) => res.json())
+    .then(handleResponse)
     .then((data) => {
         msg.innerText = data['message'];
-        console.log(data)
-        location.reload(true);
+        loadTable();
     })
-    .catch(err => console.log(err))
+    .catch(err => {
+        msg.innerHTML = err['message'];
+        if (err['msg'] === 'Token has expired') {
+            msg.innerHTML = 'lets log him out';
+        }
+        console.log(err)
+    })
+}
+
+function updateUser(e){
+    e.preventDefault();
+
+    const updateUserUrl = `https://thecodestoremanager-api-heroku.herokuapp.com/api/v1/users/${user_id}`
+    let name = document.getElementById('updateName').value;
+    let username = document.getElementById('updateUsername').value;
+    let password = document.getElementById('updatePassword').value;
+    let role = document.getElementById('role').value;
+
+    fetch(updateUserUrl, {
+        method: 'PUT',
+        mode: "cors",
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer '+ token 
+        },
+        body:JSON.stringify({
+            name:name, username:username, password:password, role:role
+        })
+    })
+    .then(handleResponse)
+    .then((data) => {
+        updateMsg.innerText = data['message'];
+        loadTable();
+    })
+    .catch(err => {
+        updateMsg.innerHTML = err['message'];
+        if (err['msg'] === 'Token has expired') {
+            updateMsg.innerHTML = 'lets log him out';
+        }
+        console.log(err)
+    })
+}
+
+//Handle response
+function handleResponse(response) {
+    return response.json()
+    .then(json => {
+        if (response.ok) {
+            return json
+        } else {
+            return Promise.reject(json)
+        }
+    })
 }
 
 //On window load
@@ -60,8 +114,7 @@ function loadTable() {
             'Authorization': 'Bearer '+ token 
         },
     })
-    .then(errHandler)
-    .then((res) => res.json())
+    .then(handleResponse)
     .then((data) => {
         populateTable(data['users']);
     })
@@ -83,7 +136,7 @@ function populateTable(users) {
 function drawTable(user) {
     var row = $('<tr />')
     var select = '<input type="checkbox">'
-    var edit = '<i class="fa fa-edit" onclick="editRow()"></i>'
+    var edit = '<i class="fa fa-edit" onclick="editRow(this)"></i>'
     var del = '<i class="fa fa-close" onclick="deleteRow(this)"></i>'
     $('#mytable').append(row);
     row.append($('<td>' + select + '</td>'));
@@ -103,5 +156,18 @@ function deleteRow(call) {
 
 //Edit table row
 function editRow(call) {
+    var table = document.getElementById('mytable')
+    var userForm = document.forms['updateUser']
+    var name = userForm.elements[0]
+    var username = userForm.elements[1]
+    var data = [];
+    var i = call.parentNode.parentNode.rowIndex;
+    user_id = table.rows[i].cells[1].innerHTML
+    for (var c = 2; c < table.rows[i].cells.length - 3; c++){
+        content = table.rows[i].cells[c].innerHTML;
+        data.push(content)
+    }
+    name.value = data[0];
+    username.value = data[1];
     $('form').animate({height: "toggle", opacity: "toggle"}, 'slow');
 }
